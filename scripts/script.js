88b79e57d6bca3540ec1c235d33dd646fa81ce47
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-02-03 21:26:59 +0100

    add some js for darkmode toggle

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,5 @@
-// JavaScript Document
+const theme = document.getElementById("theme");
+theme.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    theme.textContent === "🌞" ? theme.textContent = "🌚" : theme.textContent = "🌞"
+})
