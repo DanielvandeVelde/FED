cdb646f0ccee80556e3a26428575335f8ba42b14
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-03-10 10:54:04 +0100

    automatic darkmode for users that have that as their browser preference

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,16 @@
-const theme = document.getElementById("theme");
-theme.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    theme.textContent === "🌞" ? theme.textContent = "🌚" : theme.textContent = "🌞"
+const themeButton = document.getElementsByTagName("button")[0];
+const firstSpan = document.getElementsByTagName("span")[0];
+
+//Check if user prefers darkmode
+if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  document.body.classList.add("dark");
+  themeButton.textContent = "🌚";
+}
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  themeButton.textContent === "🌞" ? themeButton.textContent = "🌚" : themeButton.textContent = "🌞"
 })
+
+const currentDate = new Date().toLocaleDateString('nl-nl', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+firstSpan.textContent = currentDate;
