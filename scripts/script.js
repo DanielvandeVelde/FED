df9e2f243bce89dbbb118be704311c904c63819c
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-03-10 12:51:06 +0100

    minor changes and removal of spoilers for next week

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 const themeButton = document.getElementsByTagName("button")[0];
-const firstSpan = document.getElementsByTagName("span")[0];
 
 //Check if user prefers darkmode
 if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
@@ -11,6 +10,3 @@ themeButton.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   themeButton.textContent === "🌞" ? themeButton.textContent = "🌚" : themeButton.textContent = "🌞"
 })
-
-const currentDate = new Date().toLocaleDateString('nl-nl', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
-firstSpan.textContent = currentDate;
